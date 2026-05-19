5c935b3d73270656bfbc466e944db6a0787eda1f
Tanya Chauhan <tanyachauhan0628@gmail.com>
2026-05-19 16:00:20 +0530

    Implement FAQ toggle functionality
    Added functionality to toggle FAQ items when clicked.

--- script.js
+++ script.js
@@ -15,3 +15,18 @@ menuBtn.addEventListener("click", () => {
   }
 
 });
+/* FAQ TOGGLE */
+
+const faqItems = document.querySelectorAll(".faq-item");
+
+faqItems.forEach((item) => {
+
+  const question = item.querySelector(".faq-question");
+
+  question.addEventListener("click", () => {
+
+    item.classList.toggle("active");
+
+  });
+
+});
